0732f4dbbb64a63555e38afd638fa783169357a9
Zodac <devblueb@gmail.com>
2025-10-17 12:35:06 -0400

    Update example.m

--- example.m
+++ example.m
@@ -1,30 +1,48 @@
-#import <Cocoa/Cocoa.h>
+#import <UIKit/UIKit.h>
+
+@interface HiWindow : UIWindow
+@end
+
+@implementation HiWindow {
+    CGPoint initialTouchPoint;
+}
+
+- (instancetype)init {
+    self = [super initWithFrame:CGRectMake(100, 100, 120, 50)];
+    if (self) {
+        self.windowLevel = UIWindowLevelAlert + 1;
+        self.backgroundColor = [UIColor clearColor];
+        self.hidden = NO;
+
+        UIButton *hiButton = [UIButton buttonWithType:UIButtonTypeSystem];
+        hiButton.frame = self.bounds;
+        [hiButton setTitle:@"Hi" forState:UIControlStateNormal];
+        hiButton.backgroundColor = [UIColor colorWithWhite:1 alpha:0.9];
+        hiButton.layer.cornerRadius = 10;
+        [self addSubview:hiButton];
+
+        [self makeKeyAndVisible];
+    }
+    return self;
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    initialTouchPoint = [[touches anyObject] locationInWindow];
+}
+
+- (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    CGPoint currentPoint = [[touches anyObject] locationInWindow];
+    CGPoint offset = CGPointMake(currentPoint.x - initialTouchPoint.x, currentPoint.y - initialTouchPoint.y);
+    CGRect frame = self.frame;
+    frame.origin.x += offset.x;
+    frame.origin.y += offset.y;
+    self.frame = frame;
+}
+@end
 
 __attribute__((constructor))
-static void showHiWindow() {
+static void showHi() {
     dispatch_async(dispatch_get_main_queue(), ^{
-        NSApplication *app = [NSApplication sharedApplication];
-        [app setActivationPolicy:NSApplicationActivationPolicyRegular];
-
-        NSWindow *window = [[NSWindow alloc]
-            initWithContentRect:NSMakeRect(0, 0, 200, 100)
-                      styleMask:(NSWindowStyleMaskTitled |
-                                 NSWindowStyleMaskClosable |
-                                 NSWindowStyleMaskResizable)
-                        backing:NSBackingStoreBuffered
-                          defer:NO];
-
-        NSTextField *label = [[NSTextField alloc] initWithFrame:NSMakeRect(70, 40, 60, 20)];
-        [label setStringValue:@"Hi"];
-        [label setBezeled:NO];
-        [label setDrawsBackground:NO];
-        [label setEditable:NO];
-        [label setSelectable:NO];
-        [[window contentView] addSubview:label];
-
-        [window center];
-        [window makeKeyAndOrderFront:nil];
-        [app activateIgnoringOtherApps:YES];
-        [app run];
+        HiWindow *window = [[HiWindow alloc] init];
     });
 }
